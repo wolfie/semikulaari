585559871f9cb3a3a708f9ac80cc670f0324d6e1
Henrik Paul <henrik.paul@gmail.com>
2023-11-17 19:47:26 +0200

    use vite-plugin-mkcert for https

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import mkcert from 'vite-plugin-mkcert';
 import { defineConfig } from 'vite';
-import fs from 'fs';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), mkcert()],
 	server: {
-		https: {
-			key: fs.readFileSync(`${__dirname}/cert/key.pem`),
-			cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
-		}
+		https: true
 	},
 	optimizeDeps: { needsInterop: ['howler'] }
 });
